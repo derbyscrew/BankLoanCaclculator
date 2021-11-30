d16e09062801d133dbdce10901531a1e628710be
George Small <you@example.com>
2021-11-30 11:15:48 +0000

    add loan calculation functionality

--- dist/app.js
+++ dist/app.js
@@ -60,4 +60,34 @@ class Section {
 
 const amountSection = new Section(getElement(".amount-section"), 10000, 100, 1000, 50000);
 const termSection = new Section(getElement(".term-section"), 24, 6, 12, 60);
-const interestSection = new Section(getElement(".interest-section"), 5, 0.1, 1, 10);
+const interestSection = new Section(getElement(".interest-section"), 5, 0.5, 1, 10);
+const loanForm = getElement(".loan-form");
+const monthlyResult = getElement(".month-result");
+const totalResult = getElement(".total-result");
+const interestResult = getElement(".interest-result");
+
+const calculateLoan = (e) => {
+  // Input Results
+  const amount = amountSection.input.value;
+  const years = termSection.input.value;
+  const interest = interestSection.input.value;
+  //  Calculate Variables
+  const principal = parseFloat(amount);
+  const calculateInterest = parseFloat(interest) / 100 / 12;
+  const calculatedPayments = parseFloat(years) * 12;
+  // Calculate Monthly Repayment
+  const x = Math.pow(1 + calculateInterest, calculatedPayments);
+  const monthly = (principal * x * calculateInterest) / (x - 1);
+  const monthlyPayment = monthly.toFixed(2);
+  // Calculate Interest
+  const totalInterest = (monthly * calculatedPayments - principal).toFixed(2);
+  // Calculate Total Payment
+  const totalPayment = (monthly * calculatedPayments).toFixed(2);
+  // Display Results
+  monthlyResult.innerHTML = "£" + monthlyPayment;
+  totalResult.innerHTML = "£" + totalPayment;
+  interestResult.innerHTML = "£" + totalInterest;
+  e.preventDefault();
+}
+
+loanForm.addEventListener("submit", calculateLoan);
